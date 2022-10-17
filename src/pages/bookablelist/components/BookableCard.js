3594af324d1bc12ac88e6fcf79f09de5bb7c2e09
YesunPark <lynn08082@gmail.com>
2022-10-17 23:24:19 +0900

    [Fix]: 예약카드 컴포넌트 ui완성

--- src/pages/bookablelist/components/BookableCard.js
+++ src/pages/bookablelist/components/BookableCard.js
@@ -2,23 +2,69 @@ import styled from 'styled-components';
 
 const BookableCard = ({ product }) => {
   const { name, types } = product;
+
   return (
     <CardContainer>
-      {name}
-      <br />
-      {types.map((type) => {
-        return type.type;
-      })}
+      <span className="name">{name}</span>
+      <div className="type-list">
+        {types.map((type) => {
+          return (
+            <div className="type" key={type.type}>
+              <span>{type.type}</span>
+              <ReservationBtn disabled={type.time.length ? false : true}>
+                {type.time.length ? '예약하기' : '예약마감'}
+              </ReservationBtn>
+            </div>
+          );
+        })}
+      </div>
     </CardContainer>
   );
 };
 
 const CardContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
   width: 480px;
   margin: 0 auto 20px;
   padding: 10px 20px;
   border: 1px solid ${({ theme }) => theme.borderGray};
   border-radius: 5px;
+
+  .name {
+    margin-right: 10px;
+    font-size: 22px;
+  }
+
+  .type-list {
+    width: 160px;
+    .type {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 5px;
+    }
+  }
+`;
+
+const ReservationBtn = styled.button`
+  width: 100px;
+  height: 28px;
+  background-color: ${({ theme }) => theme.mainBgColor};
+  border: none;
+  border-radius: 3px;
+  font-weight: 700;
+  &:hover {
+    cursor: pointer;
+  }
+
+  :disabled {
+    border: none;
+    background-color: #c3c3c3;
+    &:hover {
+      cursor: default;
+    }
+  }
 `;
 
 export default BookableCard;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,4 +1,5 @@
 const mainColor = '#DA005C';
+const mainBgColor = '#da005c99';
 const borderGray = ' #D9D9D9';
 
-export default { mainColor, borderGray };
+export default { mainColor, mainBgColor, borderGray };
