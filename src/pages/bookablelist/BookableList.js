433117706edb44c2c998ff7ae2451d56c683eb33
YesunPark <lynn08082@gmail.com>
2022-10-17 22:23:29 +0900

    [Add]: 예약가능목록 목데이터로 api생성

--- src/pages/bookablelist/BookableList.js
+++ src/pages/bookablelist/BookableList.js
@@ -1,7 +1,24 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
 
 const BookableList = () => {
-  return <Container></Container>;
+  const [bookableProducts, setBookableProducts] = useState([]);
+
+  useEffect(() => {
+    axios('/data/bookableData.json').then((res) => {
+      setBookableProducts(res.data);
+    });
+  }, []);
+
+  return (
+    <Container>
+      {bookableProducts.length &&
+        bookableProducts.map((product) => {
+          return <div key={product.id}>{product.name}</div>;
+        })}
+    </Container>
+  );
 };
 
 const Container = styled.div``;
