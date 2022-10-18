8dadfdd6825825004ca2bed090166daa0ea21f7d
YesunPark <lynn08082@gmail.com>
2022-10-18 19:51:06 +0900

    [Add]: 예약페이지에서 이름, 예약종류, 예약시간 입력받는 기능 완성

--- src/pages/booking/Booking.js
+++ src/pages/booking/Booking.js
@@ -6,10 +6,17 @@ import styled from 'styled-components';
 const Booking = () => {
   const params = useParams();
   const location = useLocation();
-  const [bookingInfo, setBookingInfo] = useState({ name: '', types: '' });
-  const [selectedInfo, setSelectedInfo] = useState({ type: '', time: 0 });
+  const [bookingInfo, setBookingInfo] = useState({ name: '', types: [] });
+  const [bookableTimes, setBookableTimes] = useState([]);
+  const [userHandledInfo, setUserHandledInfo] = useState({ name: '', type: '', time: '선택해주세요' });
 
-  // console.log(params.info);
+  const getParameter = (key) => {
+    return new URLSearchParams(location.search).get(key);
+  };
+
+  useEffect(() => {
+    setUserHandledInfo({ ...userHandledInfo, type: getParameter('type') });
+  }, []);
 
   useEffect(() => {
     axios('/data/bookableData.json').then((res) => {
@@ -17,36 +24,67 @@ const Booking = () => {
       bookableDatas.forEach((data) => {
         if (data.id === Number(params.info)) {
           setBookingInfo({ name: data.name, types: data.types });
+          data.types.forEach((type) => {
+            if (type.type === userHandledInfo.type) {
+              setBookableTimes(type.time);
+            }
+          });
         }
       });
     });
-  }, []);
+  }, [userHandledInfo]);
 
-  console.log(bookingInfo);
+  console.log(userHandledInfo);
 
-  const getParameter = (key) => {
-    return new URLSearchParams(location.search).get(key);
+  const handleNameInput = (e) => {
+    setUserHandledInfo({ ...userHandledInfo, name: e.target.value });
+  };
+
+  const handleSelectBox = (e) => {
+    const { id } = e.target;
+    const { value } = e.target.options[e.target.selectedIndex];
+    if (id === 'type') {
+      setUserHandledInfo({ ...userHandledInfo, [id]: value, time: '선택해주세요' });
+    } else if (id === 'time') {
+      setUserHandledInfo({ ...userHandledInfo, [id]: value });
+    }
   };
 
   return (
     <Container>
+      <div>{bookingInfo.name}</div>
       <div>
         <span>예약자 이름</span>
-        <input />
+        <input onChange={handleNameInput} />
       </div>
       <div>
-        <span>예약 시간</span>
-        <select></select>
+        <span>예약 가능 종류</span>
+        <select id="type" value={userHandledInfo.type} onChange={handleSelectBox}>
+          {bookingInfo.types.map((type) => {
+            if (type.time.length) {
+              return <option key={type.type}>{type.type}</option>;
+            }
+          })}
+        </select>
       </div>
       <div>
-        <span>상담/진료/시술</span>
-        <select></select>
+        <span>예약 가능 시간</span>
+        <select id="time" value={userHandledInfo.time} onChange={handleSelectBox} required>
+          <option className="select-holder">선택해주세요</option>
+          {bookableTimes.map((time) => {
+            return <option key={time}>{time}:00</option>;
+          })}
+        </select>
       </div>
       <button>예약하기</button>
     </Container>
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  .select-holder {
+    display: none;
+  }
+`;
 
 export default Booking;
