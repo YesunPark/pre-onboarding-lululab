f08bcb079203dc607ce558a4eec5939d287cc932
YesunPark <lynn08082@gmail.com>
2022-10-18 15:46:00 +0900

    [Add]: url에서 예약정보를 추출해 해당하는 데이터axios로 받아오는 api 완성

--- src/pages/booking/Booking.js
+++ src/pages/booking/Booking.js
@@ -1,7 +1,50 @@
+import { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import axios from 'axios';
 import styled from 'styled-components';
 
 const Booking = () => {
-  return <Container></Container>;
+  const params = useParams();
+  const location = useLocation();
+  const [bookingInfo, setBookingInfo] = useState({ name: '', types: '' });
+  const [selectedInfo, setSelectedInfo] = useState({ type: '', time: 0 });
+
+  // console.log(params.info);
+
+  useEffect(() => {
+    axios('/data/bookableData.json').then((res) => {
+      const bookableDatas = res.data;
+      bookableDatas.forEach((data) => {
+        if (data.id === Number(params.info)) {
+          setBookingInfo({ name: data.name, types: data.types });
+        }
+      });
+    });
+  }, []);
+
+  console.log(bookingInfo);
+
+  const getParameter = (key) => {
+    return new URLSearchParams(location.search).get(key);
+  };
+
+  return (
+    <Container>
+      <div>
+        <span>예약자 이름</span>
+        <input />
+      </div>
+      <div>
+        <span>예약 시간</span>
+        <select></select>
+      </div>
+      <div>
+        <span>상담/진료/시술</span>
+        <select></select>
+      </div>
+      <button>예약하기</button>
+    </Container>
+  );
 };
 
 const Container = styled.div``;
