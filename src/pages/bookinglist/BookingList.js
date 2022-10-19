b8009fd609aeb3e9ec5bdcd8efce96dc590f0dde
YesunPark <lynn08082@gmail.com>
2022-10-20 00:36:33 +0900

    [Add]: 예약조회페이지 조회 기능 완성

--- src/pages/bookinglist/BookingList.js
+++ src/pages/bookinglist/BookingList.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-import styled, { ThemeContext } from 'styled-components';
+import styled from 'styled-components';
 import { FiSearch } from 'react-icons/fi';
 import theme from '../../styles/theme';
 
@@ -17,7 +17,11 @@ const BookingList = () => {
 
   const handleSearch = () => {
     axios('/data/bookingList.json').then((res) => {
-      console.log(res.data);
+      const bookingList = res.data;
+      const result = bookingList.filter(
+        (booking) => String(booking.bookingNum) === searchInput || booking.userName === searchInput
+      );
+      setSearchResult(result[0]);
     });
   };
 
@@ -26,34 +30,54 @@ const BookingList = () => {
       <div className="search-container">
         <input
           className="search-input"
+          value={searchInput}
           placeholder="예약자 성함 또는 예약번호"
           onKeyDown={(e) => {
             e.key === 'Enter' && handleSearch();
+          }}
+          onChange={(e) => {
+            setSearchInput(e.target.value);
           }}
         ></input>
         <FiSearch size={30} color={theme.mainColor} onClick={handleSearch} />
       </div>
       <CardContainer>
-        <div className="empty-result">예약자 성함 또는 예약번호로 검색해주세요.</div>
-      </CardContainer>
-      <CardContainer>
-        <div className="empty-result">검색 결과가 없습니다.</div>
-      </CardContainer>
-      {/* <CardContainer>
-        <div className="title">
-          <div>예약번호 :</div>
-          <div>예약자 성함 :</div>
-          <div>예약상품 :</div>
-          <div>예약시간 :</div>
+        <div className="sample">
+          검색 가능한 목데이터 :
+          <br /> 홍길동(12345), 박피부(21547),
+          <br /> 박룰루(29886), 김수분(32142),
+          <br /> 이하얀(33665), 황미백(45268)
         </div>
+      </CardContainer>
+      {searchResult !== undefined && searchResult.userName === '' && (
+        <CardContainer>
+          <div className="empty-result">예약자 성함 또는 예약번호로 검색해주세요.</div>
+        </CardContainer>
+      )}
+      {!searchResult && (
+        <CardContainer>
+          <div className="empty-result">검색 결과가 없습니다.</div>
+        </CardContainer>
+      )}
 
-        <div className="search-result">
-          <div>123345</div>
-          <div>홍길동</div>
-          <div>토닝/진료</div>
-          <div>9:00</div>
-        </div>
-      </CardContainer> */}
+      {searchResult !== undefined && searchResult.userName !== '' && (
+        <CardContainer>
+          <div className="title">
+            <div>예약번호 :</div>
+            <div>예약자 성함 :</div>
+            <div>예약상품 :</div>
+            <div>예약시간 :</div>
+          </div>
+          <div className="search-result">
+            <div>{searchResult.bookingNum}</div>
+            <div>{searchResult.userName}</div>
+            <div>
+              {searchResult.product}/{searchResult.type}
+            </div>
+            <div>{searchResult.time}:00</div>
+          </div>
+        </CardContainer>
+      )}
     </Container>
   );
 };
@@ -88,10 +112,14 @@ const CardContainer = styled.div`
   display: flex;
   justify-content: space-between;
   width: 480px;
-  margin: 0 auto;
+  margin: 10px auto 0;
   padding: 10px 20px;
   border: 1px solid ${({ theme }) => theme.borderGray};
   border-radius: 5px;
+
+  .sample {
+    line-height: 25px;
+  }
 
   .title {
     font-size: 20px;
